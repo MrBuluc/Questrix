bd05de695f21fc8023ed834a536d9947b6df3618
mrbuluc <hakkican@sozerbilgisayar.com>
2026-04-27 13:10:48 +0300

    [Feat] Mapper

---
+++ Core/Questrix.Mapper/Registration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Questrix.Application.Interfaces.AutoMapper;
+
+namespace Questrix.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
